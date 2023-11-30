459f1e98155ca51615fe2c95e45cac23fb5d3ea2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-30 09:55:27 +0200

    text

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,25 @@
 'use strict';
 console.log('fetch.js file was loaded');
+
+// Fetch API
+let url = 'https://jsonplaceholder.typicode.com/posts';
+// url = 'https://jsonplaceholder.typicode.com/users';
+let localUrl = '/db.txt';
+// fetch(url).then()
+// url = 'nezinau kas cia per adresas';
+fetch(localUrl)
+  .then((response) => {
+    console.log('response ===', response);
+    // jei response ne ok tai klaida
+    return response.text();
+    return response.json();
+  })
+  .then((dataInJs) => {
+    console.log(dataInJs);
+  })
+  .catch((err) => {
+    console.warn('klaida fetch', err);
+  });
+
+// 1. i el su id text mes norim ikelti teksta gauti is db.txt
+// 2. i el su id posts mes norim sugeneruoti postus is https://jsonplaceholder.typicode.com/posts
